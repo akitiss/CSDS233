97e894095b0df8a953730bd6623365ea61ef130c
April Li <april.li0427@gmail.com>
2023-10-02 16:21:33 -0400

    hm

---
+++ hw2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class hw2{
+
+    //Design an algorithm to find the node X and swapit with its successor node
+    static int partA(String input){
+        return null;
+    }  
+
+
+    public static void main(String[] args) {
+
+        boolean run = true;
+        Scanner in = new Scanner(System.in); //scanner to read in user input 
+
+        while (run){ //while the user hasn't quit, keep running 
+            System.out.print("Hello ");
+            String option = in.nextLine();
+        }
+        
+    }
+
+
+}
+
