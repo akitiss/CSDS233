598b46fad88069cc23c08fbaba86c7eb29b64a1d
April Li <april.li0427@gmail.com>
2023-11-27 15:34:49 -0500

    created student class

---
+++ hw5/Student.java
@@ -0,0 +1,25 @@
+public class student{
+    //private vars since don't want this information to be easily accessible
+    private String name //student name 
+    private int balance //current balance in the students account
+
+    //constructor of student 
+    public Student(String name, int balance){
+        this.name = name; 
+        this.balance = balance; 
+    }
+
+    //get balance of student 
+    public int getBalance(){ 
+        return this.balance; 
+    }
+
+    //update balance of student 
+    public void updateBalance(int newAmount){ 
+        this.balance = newAmount; 
+    }
+    
+
+
+
+}
---
+++ hw5/caseCashSystem.java
@@ -0,0 +1,9 @@
+public class caseCashSystem{
+
+    
+
+
+    public static void main(String[] args) {
+
+    }
+}
