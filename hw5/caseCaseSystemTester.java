8c0cf8e0b0b36767345c877ff9105c4b3996041c
April Li <april.li0427@gmail.com>
2023-11-28 10:38:42 -0500

    YES YES YES THE TEST WORKS

--- hw5/caseCaseSystemTester.java
+++ hw5/caseCaseSystemTester.java
@@ -16,7 +16,6 @@ public class caseCaseSystemTester{
                                         "INIT, Kim, 300",
                                         "INIT, Quyen, 400",
                                         "SORT, name");
-
         List<String> outputs = caseCase.runSimulation(inputs);
 
         List<String> answers = List.of("true",
@@ -34,6 +33,8 @@ public class caseCaseSystemTester{
                             "INIT, Luffy, 0",
                             "SORT, name");
 
+        outputs = caseCase.runSimulation(inputs);
+
         
         answers = List.of("true",
                             "true",
@@ -47,8 +48,10 @@ public class caseCaseSystemTester{
         inputs = List.of("INIT, Karissa, 800", 
                             "INIT, April, 100", 
                             "INIT, Trevor, 300", 
-                            "INIT, Sheena, 0");
+                            "INIT, Sheena, 0",
+                            "SORT, name");
 
+        outputs = caseCase.runSimulation(inputs);
         
         answers = List.of("true",
                             "true",
@@ -77,27 +80,25 @@ public class caseCaseSystemTester{
         List<String> answers = List.of("true",
                                         "true",
                                         "true",
-                                        "[Kim, Quyen, Tammy]",
                                         "[Tammy, Kim, Quyen]",
                                         "true",
-                                        "[Kim, Quyen, Tammy]",
                                         "[Kim, Tammy, Quyen]");
 
         assertArrayEquals(answers.toArray(), outputs.toArray());
 
-                //test 2
+        //test 2
         inputs = List.of("INIT, Sanji, 200", 
                             "INIT, Law, 400", 
                             "INIT, Zoro, 0", 
                             "INIT, Luffy, 0",
                             "SORT, balance");
-
+        outputs = caseCase.runSimulation(inputs);
         
         answers = List.of("true",
                             "true",
                             "true",
                             "true",
-                            "[Law, Luffy, Sanji, Zoro]");
+                            "[Luffy, Zoro, Sanji, Law]");
 
         assertArrayEquals(answers.toArray(), outputs.toArray());
 
@@ -107,13 +108,13 @@ public class caseCaseSystemTester{
                             "INIT, Trevor, 300", 
                             "INIT, Sheena, 0",
                             "SORT, balance");
-
+        outputs = caseCase.runSimulation(inputs);
         
         answers = List.of("true",
                             "true",
                             "true",
                             "true",
-                            "[April, Karissa, Sheena, Trevor]");
+                            "[Sheena, April, Trevor, Karissa]");
 
         assertArrayEquals(answers.toArray(), outputs.toArray());
 
